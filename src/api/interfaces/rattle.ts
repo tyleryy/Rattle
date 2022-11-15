3ca9cb1d6ea8efb0645d55605a21896400b9b3d2
Dylan Vu <dylanvu92gmail.com>
2022-11-14 21:32:41 -0800

    create player class instead

--- src/api/interfaces/rattle.ts
+++ src/api/interfaces/rattle.ts
@@ -1,23 +1,18 @@
+import { Player } from "../classes/player";
+
 /** interface for knowing all the game instances */
 export interface Rattle {
     [lobbyCode: string]: Game;
 }
 
-// interface for a singular game instance
+/** interface for a singular game instance */
 export interface Game {
-    p1: Player; // player 1
-    p2: Player; // player 2
+    p1: Player | null; // player 1
+    p2: Player | null; // player 2
 
     // game configuration options from frontened
 
     currRounds: number; // the round the game is currently at
     time: number; // the time per round
     totalRounds: number; // the total rounds before ending the game
-}
-
-export interface Player {
-    number: number; // should be 1 or 2
-    char: string; // the path or an identifer for the character in the frontend
-    score: number; // the numerical score
-    active: boolean; // whether the player is active or not
 }
