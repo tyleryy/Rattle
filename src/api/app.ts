43ab169aab277d82cff794dc00902fa08f5cfb49
Tyler Yu <tyler.y.yu@gmail.com>
2022-11-15 23:47:53 -0800

    moved sockets into connection io

--- src/api/app.ts
+++ src/api/app.ts
@@ -16,71 +16,82 @@ app.get('/', (req: Request, res: Response) => {
     res.sendStatus(200);
 })
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on('connection', (socket: Socket) => {
+    
+    socket.on('createLobby', () => {
+        let lobby_code = generateLobbyCode(rattle_games);
+        socket.join(lobby_code);
+        let host = new Player(1, socket.id);
+        let game: Game = {
+            p1: host,
+            p2: null,
+            GameActive: false,
+            currRounds: 0,
+            time: 0,
+            totalRounds: 0
+        }
+        rattle_games[lobby_code] = game;
+        // possibly return success or fail
+        return lobby_code; // return code so that frontend can reference the correct game/room
+    })
+
+    socket.on('joinLobby', (code) => {
+        socket.join(code);
+        let game: Game = (rattle_games[code] ?? null); // ! do updates to game var update rattle games?
+        if (!game) {
+            return "Lobby not found";
+        }
+        game.p2 = new Player(2, socket.id);
+        // send data to frontend
+        socket.to(code).emit("P2JoinedLobby", {p1char: game.p1?.char, p2char: game.p2.char});
+        return code; // return code so that frontend can reference the correct game/room
+    })
+    
+    socket.on('selectCharacter', (code, player_num, char) => {
+        let game: Game = rattle_games[code];
+        // ! may change later
+        // prevents character assignment if other player has selected it
+        if (player_num === 1 && game.p1) {
+            if (game.p2 && game.p2.char === char) {return "Player 2 has already selected that character";}
+            game.p1.char = char;
+        } else if (player_num === 2 && game.p2) {
+            if (game.p1 && game.p1.char === char) {return "Player 1 has already selcted that character";}
+            game.p2.char = char;
+        } else {
+            return "error";
+        }
+
+        socket.to(code).emit("doneSelecting", {p1char: game.p1?.char, p2char: game.p2?.char});
+        return `Player ${player_num} selected ${char}`;
+    })
+
+    socket.on('gameEnd', (code: string) => {
+        // TODO may add more cleanup
+        delete rattle_games[code];
+        socket.to(code).emit("endScreen");
+    })
+
+    socket.on('startGame', (code: string) => {
+        // TODO will add more functionality
+        let game: Game = rattle_games[code];
+        game.GameActive = true;
+        if (game.p1) {
+            game.p1.active = true;
+        } else {
+            return "Something went wrong"
+        }
+        socket.to(code).emit("goToGame");
+    })
+
+    socket.on('disconnect', () => {
+        let socket_room = io.of("/").adapter.sids.get(socket.id)
+        if (socket_room) {
+            let room = socket_room.values().next().value;
+            io.to(room).emit("disconnect-screen")
+        }
+    })
+
 });
-  
-io.on('/createlobby', (socket: Socket) => {
-    let lobby_code = generateLobbyCode(rattle_games);
-    socket.join(lobby_code);
-    let host = new Player(1, socket.id);
-    let game: Game = {
-        p1: host,
-        p2: null,
-        GameActive: false,
-        currRounds: 0,
-        time: 0,
-        totalRounds: 0
-    }
-    rattle_games[lobby_code] = game;
-    // possibly return success or fail
-    return lobby_code; // return code so that frontend can reference the correct game/room
-})
-
-io.on('/joinlobby', (socket: Socket, code: string) => {
-    socket.join(code);
-
-    let game: Game = (rattle_games[code] ?? null); // ! do updates to game var update rattle games?
-    if (!game) {
-        return "Lobby not found";
-    }
-    game.p2 = new Player(2, socket.id);
-    return code; // return code so that frontend can reference the correct game/room
-})
-
-io.on('/select-character', (socket: Socket, code: string, player_num: number, char: string) => {
-    let game: Game = rattle_games[code];
-    // ! may change later
-    // prevents character assignment if other player has selected it
-    if (player_num === 1 && game.p1) {
-        if (game.p2 && game.p2.char === char) {return "Player 2 has already selected that character";}
-        game.p1.char = char;
-    } else if (player_num === 2 && game.p2) {
-        if (game.p1 && game.p1.char === char) {return "Player 1 has already selcted that character";}
-        game.p2.char = char;
-    }
-    return `Player ${player_num} selected ${char}`;
-})
-
-io.on('/game/end', (socket: Socket, code: string) => {
-    // TODO may add more cleanup
-    delete rattle_games[code];
-    return;
-})
-
-io.on('game/begin', (socket: Socket, code: string) => {
-    // TODO will add more functionality
-    let game: Game = rattle_games[code];
-    game.GameActive = true;
-    if (game.p1) {
-        game.p1.active = true;
-    } else {
-        return "Something went wrong"
-    }
-    return;
-})
-
-
 
 server.listen(2000, () => console.log("server up"));
 
