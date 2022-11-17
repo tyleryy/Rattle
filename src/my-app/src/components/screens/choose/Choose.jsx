5bfc9b93227b828181ed65d88823bb44a602cc01
Tyler Yu <tyler.y.yu@gmail.com>
2022-11-17 00:45:43 -0800

    add choose

---
+++ src/my-app/src/components/screens/choose/Choose.jsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
+import Screen from '../../screen_bg/Screen'
+import ButtonChar from '../../buton/ButtonChar';
+import Button from '../../buton/Button';
+
+
+
+function Choose(props) {
+    const navigate = useNavigate();
+    const [socket, changeSocket] = useState(props.socket);
+    const [code, changeCode] = useState(props.lobbyCode);
+
+
+    useEffect(() => {
+
+          socket.on('Go Home', () => {
+            navigate('/');
+          })
+        
+        
+        return () => {socket.removeAllListeners()};
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>{props.code}</div>
+                <Screen image="./game_sprites/brick3.png"></Screen>
+                <div className = "choose">
+                    <Button imageEnter="./game_sprites/back.png" imageLeave="./game_sprites/back2.png" routesPath="/">dog</Button>
+                    <div>
+                        <ButtonChar imageEnter="./game_sprites/char1_but.png" imageLeave="./game_sprites/char1_but2.png"></ButtonChar>
+                    </div>
+                    <div>
+                        <ButtonChar imageEnter="./game_sprites/char2_but.png" imageLeave="./game_sprites/char2_but2.png"></ButtonChar>
+                    </div>
+                    <div>
+                    <ButtonChar imageEnter="./game_sprites/char3_but2.png" imageLeave="./game_sprites/char3_but.png"></ButtonChar>
+                    </div>
+                </div>
+            </header>
+        </div>
+
+    )
+}
+
+export default Choose;
