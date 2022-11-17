8a019fec2c4d547be01b31fb5807ec557e00f411
Casey Tran <caseymt@uci.edu>
2022-11-17 02:17:31 -0800

    sure

--- src/my-app/src/components/screens/options/Options.jsx
+++ src/my-app/src/components/screens/options/Options.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import Screen from '../../screen_bg/Screen'
 import Button from '../../buton/Button';
+import ButtonVal from '../../buton/ButtonVal';
 
 function Options(props) {
     return (
-        <div className="App">
-            <header className="App-header">
-                <Screen image="./game_sprites/brick3.png"></Screen>
-                <div className = "join">
-                    <div>
-                        <Button imageEnter="./game_sprites/back.png" imageLeave="./game_sprites/back2.png" routesPath="/">dog</Button>
+        <div className = "parent">
+                <div className = "opts">
+
+                    <div className = "column">
+                        <div>
+                            <Button imageEnter="./game_sprites/back.png" imageLeave="./game_sprites/back2.png" routesPath="/">dog</Button>
+                        </div>
+                        <div>
+                            <ButtonVal imageEnter="./game_sprites/3rounds.png" imageLeave="./game_sprites/3rounds2.png" val="3">3round</ButtonVal>
+                        </div>
+                        <div>
+                            <ButtonVal imageEnter="./game_sprites/5rounds.png" imageLeave="./game_sprites/5rounds2.png" val="5">5round</ButtonVal>
+                        </div>
+                        <div>
+                            <ButtonVal imageEnter="./game_sprites/7rounds.png" imageLeave="./game_sprites/7rounds2.png" val="7">7round</ButtonVal>
+                        </div>
                     </div>
-                        <input type="number" placeholder="Seconds only. Max 60 seconds" min="0" maxLength="2"/>
-                        <input type="submit" value="Submit" onClick={(console.log("hello"))} />
-            
+                    <div className = "column">
+                        <div>
+                            <ButtonVal imageEnter="./game_sprites/5seconds.png" imageLeave="./game_sprites/5seconds2.png" val="5">5seconds</ButtonVal>
+                        </div>
+                        <div>
+                            <ButtonVal imageEnter="./game_sprites/10seconds.png" imageLeave="./game_sprites/10seconds2.png" val="10">10seconds</ButtonVal>
+                        </div>
+                        <div>
+                            <ButtonVal imageEnter="./game_sprites/15seconds.png" imageLeave="./game_sprites/15seconds2.png" val="15">15seconds</ButtonVal>
+                        </div>
+                    </div>
                 </div>
-            </header>
+                    
+                    {/* <input type="number" placeholder="Seconds only. Max 60 seconds" min="0" maxLength="2"/>
+                        <input type="submit" value="Submit" onClick={(console.log("hello"))} /> */}
         </div>
     )
 }
