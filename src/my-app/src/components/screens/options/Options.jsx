cb7daa0901b1f94c6684f5f9f9673e066d6caeea
bphan4bryant <bphan4bryant@yahoo.com>
2022-11-17 23:52:47 -0800

    something

--- src/my-app/src/components/screens/options/Options.jsx
+++ src/my-app/src/components/screens/options/Options.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import Screen from '../../screen_bg/Screen'
 import Button from '../../buton/Button';
 import ButtonVal from '../../buton/ButtonVal';
+import BackButtonRow from "../../backButtonRow/backButtonRow";
 
 function Options(props) {
     return (
         <div className = "parent">
+            <BackButtonRow />
                 <div className = "opts">
-                        <div>
-                            <Button imageEnter="./game_sprites/back.png" imageLeave="./game_sprites/back2.png" routesPath="/">dog</Button>
-                        </div>
                         <div>
                             <Button imageEnter="./game_sprites/start.png" imageLeave="./game_sprites/start2.png" routesPath="/game">dog2</Button>
                         </div>
