fe76214b869d6902abeeecbd6aa4c07e882c4898
Casey Tran <caseymt@uci.edu>
2022-11-18 01:41:10 -0800

    adding buton and screens

--- src/my-app/src/components/screens/victory/victory.tsx
+++ src/my-app/src/components/screens/victory/victory.tsx
@@ -10,12 +10,15 @@ function Victory() {
         <div className="page" >
             <BackButtonRow />
             <div className="column">
+                    <div className = "winner">
+                        <img src="./game_sprites/winner.png" id="winner"/>
+                    </div>
                     <div className = "stage">
-                        <Stage width={400} height={700} options={{ backgroundAlpha:0 }}>
+                        <Stage width={400} height={710} options={{ backgroundAlpha:0 }}>
                             {/* <Sprite image={player1.image} scale={ {x: 0.8 , y: 0.8} }/> */}
-                            <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char1.png", "./game_sprites/char1frame2.png"]} anchor={0.01} visible = {true}/> 
-                            <AnimatedSprite animationSpeed={0.05} isPlaying={false} images={["./game_sprites/char2_frames/char2frame1.png", "./game_sprites/char2_frames/char2frame3.png"]} anchor={0.01} visible={false}/>
-                            <AnimatedSprite animationSpeed={0.05} isPlaying={false} images={["./game_sprites/petrframe1.png", "./game_sprites/char3frame2.png"]} anchor={0.01} visible={false}/>
+                            <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char1.png", "./game_sprites/char1frame2.png"]} anchor={0.02} visible = {true}/> 
+                            <AnimatedSprite animationSpeed={0.05} isPlaying={false} images={["./game_sprites/char2_frames/char2frame1.png", "./game_sprites/char2_frames/char2frame3.png"]} anchor={0.02} visible={false}/>
+                            <AnimatedSprite animationSpeed={0.05} isPlaying={false} images={["./game_sprites/petrframe1.png", "./game_sprites/char3frame2.png"]} anchor={0.02} visible={false}/>
                         </Stage>
                     </div>
                     <img src="./game_sprites/platform2.png"/>
