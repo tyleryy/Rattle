0b99a2aebb9ae22d26df519783e4b512c96c2a8f
Tyler Yu <tyler.yu@pacificlife.com>
2022-12-21 23:30:33 -0800

    cleaned up home screen html and styles

--- src/my-app/src/components/screens/home/Home.jsx
+++ src/my-app/src/components/screens/home/Home.jsx
@@ -9,11 +9,11 @@ import { useNavigate } from "react-router-dom";
 
 
 const Home = () => {
+  // get global vars of game state
   const states = useContext(Context);
   const [lobby_code, changeLobbyCode] = states.lobby_state;
   const [player, changePlayer] = states.player_state;
   const [socket, _] = states.socket_state;
-  // const socket = props.socket;
   const navigate = useNavigate();
   useEffect(() => {
 
@@ -32,36 +32,31 @@ const Home = () => {
 
   return (
     <div className="homeParent">
-      <div className="title-row">
-        <Title class="title-shaking"></Title>
-      </div>
+      {/* Animated Rattle Title at top of Home screen */}
+      <Title></Title>
       <div className="home">
-        <div className="Sprite">
-          <Stage width={300} height={700} options={{ backgroundAlpha: 0 }}>
-            {/* {p1Ani} */}
-            <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char2_frames/char2frame1.png", "./game_sprites/char2_frames/char2frame3.png"]} anchor={0.01} />
-            {/* <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char2.png", "./game_sprites/back2.png"]} anchor={0.5}/> */}
-          </Stage>
-        </div>
-        <div className="button1">
-          <Button imageEnter="./game_sprites/create2.png" imageLeave="./game_sprites/create.png" routesPath="/choose" socketEmitEvent={"createLobby"} socket={socket}>dog</Button>
-        </div>
-        <div className="button1">
-          <Button imageEnter="./game_sprites/join.png" imageLeave="./game_sprites/join2.png" routesPath="/join" socket={socket}>dog</Button>
-        </div>
-        <div>
-          <Button imageEnter="./game_sprites/credits.png" imageLeave="./game_sprites/credits2.png" routesPath="/credit">dog</Button>
-        </div>
-          <div className = "Sprite">
-              <Stage width={300} height={700} options={{ backgroundAlpha:0 }}>
-                {/* {p1Ani} */}
-                <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char1.png", "./game_sprites/char1frame2.png"]} anchor={0.01}/> 
-                {/* <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char2.png", "./game_sprites/back2.png"]} anchor={0.5}/> */}
-              </Stage>
-            </div> 
-      </div>
-      <div className = "petr">
-          <ButtonPetr imageEnter="./game_sprites/char3.png" imageLeave="./game_sprites/char3.png">petr!</ButtonPetr>
+        {/* Character model on left of screen */}
+        <Stage width={300} height={700} options={{ backgroundAlpha: 0 }}>
+          {/* {p1Ani} */}
+          <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char2_frames/char2frame1.png", "./game_sprites/char2_frames/char2frame3.png"]} anchor={0.01} />
+          {/* <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char2.png", "./game_sprites/back2.png"]} anchor={0.5}/> */}
+        </Stage>
+
+        {/* main 3 buttons: create lobby, join lobby, credits */}
+        <Button imageEnter="./game_sprites/create2.png" imageLeave="./game_sprites/create.png" routesPath="/choose" socketEmitEvent={"createLobby"} socket={socket}>dog</Button>
+        <Button imageEnter="./game_sprites/join.png" imageLeave="./game_sprites/join2.png" routesPath="/join" socket={socket}>dog</Button>
+        <Button imageEnter="./game_sprites/credits.png" imageLeave="./game_sprites/credits2.png" routesPath="/credit">dog</Button>
+        
+        {/* Character model on right of screen */}
+        <Stage width={300} height={700} options={{ backgroundAlpha:0 }}>
+          {/* {p1Ani} */}
+          <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char1.png", "./game_sprites/char1frame2.png"]} anchor={0.01}/> 
+          {/* <AnimatedSprite animationSpeed={0.05} isPlaying={true} images={["./game_sprites/char2.png", "./game_sprites/back2.png"]} anchor={0.5}/> */}
+        </Stage>
+      </div> 
+      {/* must wrap petr in div because flex must be in an element supporting flexbox like div */}
+      <div className="petr"> 
+        <ButtonPetr imageEnter="./game_sprites/char3.png" imageLeave="./game_sprites/char3.png">petr!</ButtonPetr>
       </div>
     </div>
   )
